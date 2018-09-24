32dea6842593765009f2cd020307a3f6d8a681fc
Valorn <valornthered@gmail.com>
2018-09-24 07:45:24 -0500

    Added UI elements
    Added UI text elements for tracking the speed, score, and eventually for distance travelled.

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -5,18 +5,44 @@ using UnityEngine;
 public class GameMaster : MonoBehaviour
 {
 
-	//private, inspector variables
+	//inspector variables
 	[SerializeField] float gameSpeed = 1.0f; //holder for current object speed, determines inital speed of startup
 	[SerializeField] float acceleration = 0.1f; //amount speed is increased per update
+	[SerializeField] float scoreTime = 1f; //timer variable for updating score
+
+	//hidden variables
+	private int score = 0; //current score earned by player
+	private float scoreTimer = 0f; 
+	private float distance = 0f; //current distance travelled by player
+	private int floor = 1; //current floor player is on
 
 	void Update ()
 	{
 		gameSpeed += acceleration; //increment object speed by acceleration
+		scoreTimer += Time.deltaTime; //increment time in real time
+
+		if(scoreTimer >= scoreTime) //when timer hits the right time
+		{
+			score += 1 * Mathf.Abs(floor); //increment score by floor number
+			scoreTimer = 0f; //reset timer
+		}
 	}
 
 	public float Speed ()
 	{
-		return gameSpeed; //Getter used for setting the speed of all objects
+		float absSpeed; //temp variable for display speed
+
+		absSpeed = Mathf.Round(gameSpeed); //rounds off speed to avoid display errors
+		return absSpeed; //Getter used for setting the speed of all objects
 	}
 
+	public int Score ()
+	{
+		return score; //getter used for updating score display
+	}
+
+	public float Distance ()
+	{
+		return distance; //getter used for updating distance display
+	}
 }
---
+++ Assets/Scripts/SpeedText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedText : MonoBehaviour {
+
+	GameMaster gm; //holder for gamemaster
+	Text text; //holder for ui text
+	
+	private string startText; //holder for label set in inspector
+	
+	void Start () {
+		gm = FindObjectOfType<GameMaster>(); //assign gamemaster to holder
+		text = GetComponent<Text>(); //assign ui text to holder
+		startText = text.text;	//copy label text to holder
+	}
+	
+	void Update () {
+		text.text = "" + startText + gm.Speed(); //display label and speed value
+	}
+}
