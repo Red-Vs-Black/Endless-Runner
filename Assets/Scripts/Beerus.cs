76ccfeeb6dfe41eb056d75270024bc21aba68ca5
Valorn <valornthered@gmail.com>
2018-10-02 15:29:03 -0500

    Upgrade to Unity2018.2.10f and VisualStudio

---
+++ Assets/Scripts/Beerus.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beerus : MonoBehaviour {
+
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        Destroy(col.gameObject);
+    }
+}
