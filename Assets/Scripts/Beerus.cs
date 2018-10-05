c1454d49d126ea313400f12c9bea67c37deec324
Valorn <valornthered@gmail.com>
2018-10-05 00:04:00 -0500

    Update for new version and physics fix
    Made TheDestroyer and World Objects into trigger colliders, to prevent them from colliding with the player and haulting.

--- Assets/Scripts/Beerus.cs
+++ Assets/Scripts/Beerus.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Beerus : MonoBehaviour {
 
-    void OnCollisionEnter2D (Collision2D col)
+    void OnTriggerEnter2D (Collider2D col)
     {
         Destroy(col.gameObject);
     }
