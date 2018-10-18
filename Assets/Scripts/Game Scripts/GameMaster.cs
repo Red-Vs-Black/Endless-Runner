7f37089dae7f4fc529d44d8e06607a7b12a81089
Valorn <valornthered@gmail.com>
2018-10-18 18:31:23 -0500

    Created Death

--- Assets/Scripts/Game Scripts/GameMaster.cs
+++ Assets/Scripts/Game Scripts/GameMaster.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {
-
+	Player thePlayer; //reference for the player script
+	public GameObject deathCanvas; //reference for the game over text
+	
 	//inspector variables
 	[SerializeField] bool enableAcceleration = true; //toggle for gameSpeed acceleration
 	[SerializeField] float gameSpeed = 1.0f; //holder for current object speed, determines inital speed of startup
@@ -16,12 +18,19 @@ public class GameMaster : MonoBehaviour
 	private float scoreTimer = 0f; 
 	private float distance = 0f; //current distance travelled by player
 	private int floor = 1; //current floor player is on
+	private float curGameSpeed; //holder for current object speed, determines inital speed of startup
 
+	void Start ()
+	{
+		curGameSpeed = gameSpeed; //save initial game speed
+		thePlayer = FindObjectOfType<Player> (); //populate player reference
+	}
+	
 	void Update ()
 	{
 		if (enableAcceleration)
 		{
-			gameSpeed += acceleration; //increment object speed by acceleration
+			curGameSpeed += acceleration; //increment object speed by acceleration
 		}
 		
 		scoreTimer += Time.deltaTime; //increment time in real time
@@ -32,12 +41,22 @@ public class GameMaster : MonoBehaviour
 			scoreTimer = 0f; //reset timer
 		}
 	}
-
+	
+	public void ModSpeed (float mod) //function to change the game speed
+	{
+		curGameSpeed += mod;
+	}
+	
+	public void Stop () //function to stop the game.
+	{
+		curGameSpeed = 0f;
+	}
+	
 	public float Speed ()
 	{
 		float absSpeed; //temp variable for display speed
 
-		absSpeed = Mathf.Round(gameSpeed); //rounds off speed to avoid display errors
+		absSpeed = Mathf.Round(curGameSpeed); //rounds off speed to avoid display errors
 		return absSpeed; //Getter used for setting the speed of all objects
 	}
 
@@ -49,5 +68,15 @@ public class GameMaster : MonoBehaviour
 	public float Distance ()
 	{
 		return distance; //getter used for updating distance display
+	}
+	
+	public void Restart ()
+	{
+		deathCanvas.SetActive (false); //hide gameover text
+		//TODO delete all non-player entities
+		thePlayer.gameObject.SetActive (true); //restore the player
+		thePlayer.ResetHealth (); //fill health to full
+		thePlayer.ResetPosition (); //send player back to default position
+		curGameSpeed = gameSpeed; //reset game speed
 	}
 }
--- Assets/Scripts/Game Scripts/Player.cs
+++ Assets/Scripts/Game Scripts/Player.cs
@@ -13,40 +13,63 @@ public class Player : MonoBehaviour
 
 	//private, hidden variables
 	private int curHealth; //player current hp
+	private Vector3 startPosition; //holder for starting position
 
+	public GameObject deathCanvas; //reference to game over text
+	GameMaster gm; //reference to the gm
+	
 	//getters and setters
-	public int GetHealth() //reports player current hp when called
+	public int GetHealth () //reports player current hp when called
 	{
 		return curHealth;
 	}
 
-	public void ModHealth(int mod) //modifies player hp by mod
+	public void ModHealth (int mod) //modifies player hp by mod
 	{
 		curHealth += mod;
 
-		if(curHealth > maxHealth) //check for overheal
+		if (curHealth > maxHealth) //check for overheal
 		{
 			curHealth = maxHealth; //prevent overheal
 		}
-		else if(curHealth <= 0) //check for death
+		else if (curHealth <= 0) //check for death
 		{
-			//TODO write Death function
+			Death ();
 		}
 	}
 
-	public float GetJump() //reports jump force when called
+	public void ResetHealth ()
+	{
+		curHealth = maxHealth; //refill health to full
+	}
+	
+	public float GetJump () //reports jump force when called
 	{
 		return jumpPower;
 	}
 
-	public void SetJump(float mod) //modifies jump force by mod
+	public void SetJump (float mod) //modifies jump force by mod
 	{
 		jumpPower += mod; //TODO remove if unused
 	}
 
-	void Start()
+	public void ResetPosition () //function to return player to initial position
 	{
+		transform.position = startPosition;
+	}
+	
+	void Start ()
+	{
+		gm = FindObjectOfType<GameMaster> (); //populate gm reference
 		curHealth = maxHealth; //initializes player health at max
+		startPosition = transform.position; //save initial position
 	}
 
+	void Death ()
+	{
+		deathCanvas.SetActive (true); //display game over text
+		gameObject.SetActive (false); //delete the player
+		gm.Stop(); //stop game
+	}
+	
 }
